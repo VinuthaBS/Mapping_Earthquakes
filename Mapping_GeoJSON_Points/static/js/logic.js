6dd54c3f5680fe6f8733d1d8cba2704eb084a437
Vinutha Phani <vinuthaphani@Vinuthas-MacBook-Pro.local>
2021-02-08 20:14:25 -0500

    Updated logic.js with code for multi point Airport GeoJSON data.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,30 +3,55 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      37.5, -122.5
+      30, 30
     ],
-    zoom: 10
+    zoom: 2
   });
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'streets' tile layer to the map.
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/VinuthaBS/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+   onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "<hr>Airport name: " + feature.properties.name + "</h2>");
+   } 
+  }).addTo(map);
+});
+
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
 // // Grabbing our GeoJSON data and adding popup using pointToLayer
 // L.geoJson(sanFranAirport, {
@@ -38,20 +63,10 @@ let sanFranAirport =
 //   }
 // }).addTo(map);
 
-// Grabbing GeoJSON data and adding popup using onEachFeature
-L.geoJson(sanFranAirport, {
-  onEachFeature: function(feature,layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "<hr>Airport name: " + feature.properties.name + "</h2>");
-  }
-}).addTo(map);
-
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
-// Then we add our 'streets' tile layer to the map.
-streets.addTo(map);
+// // Grabbing GeoJSON data and adding popup using onEachFeature
+// L.geoJson(sanFranAirport, {
+//   onEachFeature: function(feature,layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "<hr>Airport name: " + feature.properties.name + "</h2>");
+//   }
+// }).addTo(map);
